6b787f637fa26cefc19320fdaeb22e202af2463a
Gabriel <122808379+ajayigabriel@users.noreply.github.com>
2024-03-05 12:42:30 +0000

    intial commit

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-require('dotenv').config();
 //Import the environment variable
-const express = require('express');
+require("dotenv").config();
+
+//Import express module
+const express = require("express");
 
 // Create an instance of the express app
 const app = express();
 
 // Home route
-app.get('/', (req, res) => {
-    res.send((201).json({message:"🚀🚀🚀🚀🚀"}))
-})
+app.get("/", (req, res) => {
+  res.send((201).json({ message: "🚀🚀🚀🚀🚀" }));
+});
 
 //Import PORT from environment variable
 const PORT = process.env.PORT;
-//listen to the server 
+
+//listen to the server
 app.listen(PORT, () => {
-    console.log(`Server is live and running on PORT ${PORT}`)
-})
+  console.log(`Server is live and running on PORT ${PORT}`);
+});
